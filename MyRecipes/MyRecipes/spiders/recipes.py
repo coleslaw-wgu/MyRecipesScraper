803d9865d932b17c651fac83ee6e3679112367b3
Johnathan Sluder <jsluder@wgu.edu>
2020-01-15 13:36:58 -0600

    Created scraper

--- MyRecipes/MyRecipes/spiders/recipes.py
+++ MyRecipes/MyRecipes/spiders/recipes.py
@@ -5,7 +5,33 @@ import scrapy
 class RecipesSpider(scrapy.Spider):
     name = 'recipes'
     allowed_domains = ['https://www.myrecipes.com/']
-    start_urls = ['http://https://www.myrecipes.com//']
+    start_urls = [
+        'https://www.myrecipes.com/recipe/mediterranean-tuna-salad-2'
+        ]
 
     def parse(self, response):
-        pass
+        title = response.\
+            xpath('//*[@class="headline heading-content"]/text()')\
+            .extract_first()
+
+        totalTime = response.\
+            xpath('//*[@class="recipe-meta-item-body"]/text()')\
+            .extract_first().strip()
+
+        ingredients = response.\
+            xpath('//*[@class="ingredients"]/ul/li/text()').extract()
+
+        instructions = response.\
+            xpath('//*[@class="step"]/p/text()').extract()
+
+        nutrients = response.\
+            xpath('//*[@class="partial recipe-nutrition"]/ul/li/text()').\
+            extract()
+
+        yield {
+            'Title': title,
+            'TotalTime': totalTime,
+            'Ingredients': ingredients,
+            'Instructions': instructions,
+            'Nutrients': nutrients
+        }
